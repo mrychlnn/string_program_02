5af3133dde681538575b95868caa209a1c280368
Chloe <lunamarychloeanne@gmail.com>
2025-03-28 20:35:55 +0800

    input statement

--- string_manipulation_program_02/print_yes_or_no_if_the_end_part_matches_the_function_parameter_without_using_endswith_function.py
+++ string_manipulation_program_02/print_yes_or_no_if_the_end_part_matches_the_function_parameter_without_using_endswith_function.py
@@ -1,2 +1,4 @@
 # Input a complete statement
+statement = input("Enter a complete statement: ")
+
 # Print 'Yes' or 'No' if the end part matches the funtion parameter without using endswith() function
