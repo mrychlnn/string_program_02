252a466fb95b9b232dea9b68dc4bd5ec68864608
Chloe <lunamarychloeanne@gmail.com>
2025-03-28 21:36:24 +0800

    print output

--- string_manipulation_program_02/print_text_to_complete_the_number_of_characters_without_using_ljust.py
+++ string_manipulation_program_02/print_text_to_complete_the_number_of_characters_without_using_ljust.py
@@ -1,4 +1,11 @@
 # Input a text
 text = input("Enter the text: ")
 
+# Ask user for the total width
+width = int(input("Enter the total width: "))
+
 # Print space at the end of the string to complete the number of characters specifies in function parameter without using ljust() function
+if len(text) < width:
+    text = text + ' ' * (width - len(text))
+
+print("Output:", text)
