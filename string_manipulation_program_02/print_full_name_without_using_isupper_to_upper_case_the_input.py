1c3d2ae93320c7efbffccab4438c25ef7d878ede
Chloe <lunamarychloeanne@gmail.com>
2025-03-28 20:20:10 +0800

    print output

--- string_manipulation_program_02/print_full_name_without_using_isupper_to_upper_case_the_input.py
+++ string_manipulation_program_02/print_full_name_without_using_isupper_to_upper_case_the_input.py
@@ -1,4 +1,13 @@
 # Input the full name of the user
 full_name = input("Enter yout full name (incorrect casing): ")
 
+# Convert to upper case manually
+result = ""
+for character in full_name:
+    if 'a' <= character <= 'z':  
+        result += chr(ord(character) - 32)  
+    else:
+        result += character  
+
 # Print the entered full name in upper case without using isupper() function
+print("Output:", result)
