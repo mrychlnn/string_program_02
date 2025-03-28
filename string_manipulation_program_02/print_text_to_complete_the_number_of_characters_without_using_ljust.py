4a6f3a1297111d1548d3c9c3b6ff312ca56cbb78
Chloe <lunamarychloeanne@gmail.com>
2025-03-28 21:31:11 +0800

    input text

--- string_manipulation_program_02/print_text_to_complete_the_number_of_characters_without_using_ljust.py
+++ string_manipulation_program_02/print_text_to_complete_the_number_of_characters_without_using_ljust.py
@@ -1,2 +1,4 @@
 # Input a text
+text = input("Enter the text: ")
+
 # Print space at the end of the string to complete the number of characters specifies in function parameter without using ljust() function
