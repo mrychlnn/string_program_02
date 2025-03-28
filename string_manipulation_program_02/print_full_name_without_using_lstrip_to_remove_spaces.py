2afdd88689a95bbc4377023fcb8504449b9b6862
Chloe <lunamarychloeanne@gmail.com>
2025-03-28 19:17:01 +0800

    print output

--- string_manipulation_program_02/print_full_name_without_using_lstrip_to_remove_spaces.py
+++ string_manipulation_program_02/print_full_name_without_using_lstrip_to_remove_spaces.py
@@ -1,4 +1,13 @@
 # Input full name with spaces at the beginning 
-full_name = input("Enter your full name (with several spaces at the beginning: )")
+full_name = input("Enter your full name (with several spaces at the beginning): ")
 
-# Print the full name without wthout the spaces at the beginning 
+# Find the index of the first non-space character
+index = 0
+while index < len(full_name) and full_name[index] == ' ':
+    index += 1
+
+# Slice the string from the first non-space character
+remove_spaces = full_name[index:]
+
+# Print the full name without the spaces at the beginning 
+print("Output:", remove_spaces)
