88e1f56563b6a3d74928027d0c76a539d8cd2b09
Chloe <lunamarychloeanne@gmail.com>
2025-03-28 21:58:01 +0800

    print output

--- string_manipulation_program_02/print_text_at_the_center_without_using_center_function.py
+++ string_manipulation_program_02/print_text_at_the_center_without_using_center_function.py
@@ -5,3 +5,12 @@ text_input = input("Enter a text: ")
 width = int(input("Enter the total width for centering: "))
 
 # Print the centered text
+if len(text_input) >= width:
+    print(text_input)
+else:
+    total_spaces = width - len(text_input)
+    left_spaces = total_spaces // 2
+    right_spaces = total_spaces - left_spaces
+    
+    centered_text = " " * left_spaces + text_input + " " * right_spaces
+    print(centered_text)
