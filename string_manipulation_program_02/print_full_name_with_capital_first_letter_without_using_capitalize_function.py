d9964634a197693c93edc8167903a20ac702e01d
Chloe <lunamarychloeanne@gmail.com>
2025-03-28 22:23:13 +0800

    print output

--- string_manipulation_program_02/print_full_name_with_capital_first_letter_without_using_capitalize_function.py
+++ string_manipulation_program_02/print_full_name_with_capital_first_letter_without_using_capitalize_function.py
@@ -1,4 +1,8 @@
 # Input the user's full name
 full_name = input("Enter your full name (incorrect casing): ")
 
+# Convert the first letter to upper case and the rest will be lower case manually
+change_name = full_name[0].upper() + full_name[1:].lower()
+
 # Print the first letter of the full name in capital letter and the rest is small letter
+print("Output:", change_name)
