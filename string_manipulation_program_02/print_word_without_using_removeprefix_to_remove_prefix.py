53616b5abf5af73acb522f55a58a8de1690e8f17
Chloe <lunamarychloeanne@gmail.com>
2025-03-28 19:44:02 +0800

    print output

--- string_manipulation_program_02/print_word_without_using_removeprefix_to_remove_prefix.py
+++ string_manipulation_program_02/print_word_without_using_removeprefix_to_remove_prefix.py
@@ -1,7 +1,11 @@
 # Input a word
 word = input("Enter a word: ")
 
-# Ask the user what prefic to be removed
+# Ask the user what prefix to be removed
 prefix = input("Enter the prefix to remove: ")
 
+if word.startswith(prefix):
+    word = word[len(prefix):]
+
 # Print the entered word without prefix
+print("Output:", word)
