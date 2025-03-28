fc6a0eb61951d4f534dbe9a1cdae4e0c2055af96
Chloe <lunamarychloeanne@gmail.com>
2025-03-28 22:45:58 +0800

    print output

--- string_manipulation_program_02/print_full_name_with_all_capital_first_letter_per_word_without_using_title_function.py
+++ string_manipulation_program_02/print_full_name_with_all_capital_first_letter_per_word_without_using_title_function.py
@@ -1,4 +1,11 @@
 # Input the user's full name
 full_name = input("Enter your full name (incorrect casing): ")
+words = full_name.split()  
+capital_words = []
 
 # Print the entered full name in all capital first letters in each word without using title() function
+for word in words:
+    capital_words.append(word.capitalize()) 
+
+change_name = ' '.join(capital_words)  
+print("Output:", change_name)
