37b57fc21422fc692a3ddad3f0a935caeb1043e6
Chloe <lunamarychloeanne@gmail.com>
2025-03-31 22:10:31 +0800

    input suffix

--- string_manipulation_program_02/print_word_without_using_removesuffix_to_remove_suffix.py
+++ string_manipulation_program_02/print_word_without_using_removesuffix_to_remove_suffix.py
@@ -1,4 +1,7 @@
 # Input a word
 word = input("Enter a word: ")
 
+# Ask the user what prefix to be removed
+suffix = input("Enter the suffix to remove: ")
+
 # Print the word without the specific suffix
