7a812c790f919ad912ddfea001b6f32816a579dc
Chloe <lunamarychloeanne@gmail.com>
2025-04-01 01:28:59 +0800

    input statement

--- string_manipulation_program_03/print_yes_or_no_if_the_beginning_part_matches_the_function_parameter_without_using_startwith_function.py
+++ string_manipulation_program_03/print_yes_or_no_if_the_beginning_part_matches_the_function_parameter_without_using_startwith_function.py
@@ -1,2 +1,4 @@
 # Input a complete statement
+statement = input("Enter a complete statement: ")
+
 # Print 'Yes' or 'No' if the beginning part matches the funtion parameter without using startswith() function
