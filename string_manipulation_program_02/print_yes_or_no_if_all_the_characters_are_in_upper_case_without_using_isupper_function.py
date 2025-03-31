5ed5074f739ded1442a78508817e483f1cb89f48
Chloe <lunamarychloeanne@gmail.com>
2025-04-01 00:27:30 +0800

    print output

--- string_manipulation_program_02/print_yes_or_no_if_all_the_characters_are_in_upper_case_without_using_isupper_function.py
+++ string_manipulation_program_02/print_yes_or_no_if_all_the_characters_are_in_upper_case_without_using_isupper_function.py
@@ -2,3 +2,11 @@
 first_name = input("Enter your first name: ")
 
 # Print 'Yes" or 'No' if the name is in all capital letter or not
+all_caps = "Yes, the entered name is all in upper case"
+for char in first_name:
+    if 'a' <= char <= 'z': 
+        all_caps = "No, the entered name contains a small letter/s"
+        break
+
+# Print result
+print(all_caps)
