94a8db72db730d7b4ea693f15b0c714cac98adee
Chloe <lunamarychloeanne@gmail.com>
2025-04-01 01:47:16 +0800

    print output

--- string_manipulation_program_03/print_yes_or_no_if_the_beginning_part_matches_the_function_parameter_without_using_startwith_function.py
+++ string_manipulation_program_03/print_yes_or_no_if_the_beginning_part_matches_the_function_parameter_without_using_startwith_function.py
@@ -1,4 +1,11 @@
 # Input a complete statement
 statement = input("Enter a complete statement: ")
 
-# Print 'Yes' or 'No' if the beginning part matches the funtion parameter without using startswith() function
+# Ask the user what to check in the beginning part
+beginning_part = input("Enter the beginning to check: ")
+
+# Print 'Yes' or 'No' if the beginning part matches the function parameter without using startswith() function
+if statement[:len(beginning_part)] == beginning_part:
+    print(f"Yes. It starts with {beginning_part}")
+else:
+    print(f"No. It doesn't start with {beginning_part} ")
