fe45ce17118031ceb01606fca3e4eaae85d63b4d
Chloe <lunamarychloeanne@gmail.com>
2025-03-31 22:27:39 +0800

    print changes

--- string_manipulation_program_02/print_yes_or_no_if_the_end_part_matches_the_function_parameter_without_using_endswith_function.py
+++ string_manipulation_program_02/print_yes_or_no_if_the_end_part_matches_the_function_parameter_without_using_endswith_function.py
@@ -6,6 +6,6 @@ ending_part = input("Enter the ending to check: ")
 
 # Print 'Yes' or 'No' if the end part matches the funtion parameter without using endswith() function
 if statement[-len(ending_part):] == ending_part:
-    print("Yes")
+    print(f"Yes. It ends with {ending_part}")
 else:
-    print("No")
+    print(f"No. It doesn't end with {ending_part} ")
