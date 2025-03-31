d4203d0d08a97de36f8090dd68d2e04f4df3830a
Chloe <lunamarychloeanne@gmail.com>
2025-03-31 21:58:06 +0800

    print output

--- string_manipulation_program_02/print_full_name_without_using_rstrip_to_remove_spaces.py
+++ string_manipulation_program_02/print_full_name_without_using_rstrip_to_remove_spaces.py
@@ -1,4 +1,13 @@
 # Input the full name with trailing spaces
 full_name = input("Enter your full name (with trailing spaces): ")
 
+# Find the index of the first non-space character
+index = len(full_name) - 1
+while index >= 0 and full_name[index] == ' ':
+    index -= 1
+
+# Slice the string from the first non-space character
+remove_spaces = full_name[:index + 1]
+
 # Print the full name without the spaces at the end
+print("Output:", remove_spaces)
