120a82f9b5b72b50317062204740d47ebf72cd88
Chloe <lunamarychloeanne@gmail.com>
2025-04-01 01:02:03 +0800

    print output

--- string_manipulation_program_03/print_yes_or_no_if_all_the_characters_are_in_lower_case_without_using_islower_function.py
+++ string_manipulation_program_03/print_yes_or_no_if_all_the_characters_are_in_lower_case_without_using_islower_function.py
@@ -1,4 +1,12 @@
 # Input a first name
-first_name = input("Enter your full name: ")
+first_name = input("Enter your first name: ")
 
 # Print 'Yes' or 'No' if the name is in all lower case
+all_lower = "Yes, the entered name is all in lower case"
+for char in first_name:
+    if 'A' <= char <= 'Z':  
+        all_lower = "No, the entered name contains a big letter/s"
+        break
+
+# Print result
+print(all_lower)
