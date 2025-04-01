3501505527083548054531ded05327e0cbaf223b
Chloe <lunamarychloeanne@gmail.com>
2025-04-01 13:19:21 +0800

    input

--- string_manipulation_program_03/print_the_position_where_the_string_was_located_without_using_index_function.py
+++ string_manipulation_program_03/print_the_position_where_the_string_was_located_without_using_index_function.py
@@ -1,3 +1,7 @@
 # Input a text
+text = input("Enter a text: ")
+
 # Input the character that you want to locate
+search_word = input("Enter the character that you want to locate: ")
+
 # Print the location of the character that the user searching for
