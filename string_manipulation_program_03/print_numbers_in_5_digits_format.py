7a5b0b8d40a71f371a2c8500971e3af44e4e6abc
Chloe <lunamarychloeanne@gmail.com>
2025-04-01 12:33:55 +0800

    print output

--- string_manipulation_program_03/print_numbers_in_5_digits_format.py
+++ string_manipulation_program_03/print_numbers_in_5_digits_format.py
@@ -1,4 +1,8 @@
 # Input a 1-3 digits numbers
 num = int(input("Enter a number (0-999): "))
 
+# Format the number as 6 digits string with leading zeros
+five_digits = f"{num:05d}"
+
 # Print the number with zero characters at the beginning to complete the 5 digits format
+print("Output:", five_digits)
