aff233189b9fc7154db1bd2016cca4c5289cd376
Chloe <lunamarychloeanne@gmail.com>
2025-04-01 14:24:07 +0800

    pint output

--- string_manipulation_program_03/print_the_position_where_the_character_was_located_starting_from_last_character_without_using_rindex.py
+++ string_manipulation_program_03/print_the_position_where_the_character_was_located_starting_from_last_character_without_using_rindex.py
@@ -5,3 +5,9 @@ string = input("Enter a string: ")
 char_find = input("Enter the character that you want to locate: ")
 
 # Print The position where the last character was located
+for i in range(len(string) - 1, -1, -1):
+    if string[i] == char_find:
+        print("Output:", i)
+        break
+else:
+    print(f"'{char_find}' not found")
