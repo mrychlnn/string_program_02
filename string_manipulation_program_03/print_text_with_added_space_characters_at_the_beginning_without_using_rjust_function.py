e509bc3397ed164ab15acfda2caf503aa64cf9a8
Chloe <lunamarychloeanne@gmail.com>
2025-04-01 12:21:27 +0800

    print output

--- string_manipulation_program_03/print_text_with_added_space_characters_at_the_beginning_without_using_rjust_function.py
+++ string_manipulation_program_03/print_text_with_added_space_characters_at_the_beginning_without_using_rjust_function.py
@@ -1,4 +1,12 @@
 # Input a text
 text = input("Enter a text: ")
 
+# Ask the user for the total length
+length = int(input("Enter the total length: "))
+
 # Print space at the beginning of the text to complete the number of characters specifies in function parameter without using rjust() function
+if len(text) < length:
+    padding = " " * (length - len(text))
+    padded_text = padding + text
+
+print(f'Output: {padded_text}')
