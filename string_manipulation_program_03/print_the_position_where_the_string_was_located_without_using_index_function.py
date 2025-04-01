027eeef4c1f61f1f0f6b306f101f8f39f21bdc72
Chloe <lunamarychloeanne@gmail.com>
2025-04-01 13:30:46 +0800

    print loc

--- string_manipulation_program_03/print_the_position_where_the_string_was_located_without_using_index_function.py
+++ string_manipulation_program_03/print_the_position_where_the_string_was_located_without_using_index_function.py
@@ -2,6 +2,12 @@
 text = input("Enter a text: ")
 
 # Input the character that you want to locate
-search_word = input("Enter the character that you want to locate: ")
+search_char = input("Enter the character that you want to locate: ")
+
+position = text.find(search_char)
 
 # Print the location of the character that the user searching for
+if position != -1:
+    print("Found the character at position:", position)
+else:
+    print("The character was not found")
