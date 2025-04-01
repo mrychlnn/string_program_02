149e032deb7b3c073e8e39c789a8c522846c6567
Chloe <lunamarychloeanne@gmail.com>
2025-04-01 12:12:39 +0800

    input text

--- string_manipulation_program_03/print_text_with_added_space_characters_at_the_beginning_without_using_rjust_function.py
+++ string_manipulation_program_03/print_text_with_added_space_characters_at_the_beginning_without_using_rjust_function.py
@@ -1,2 +1,4 @@
 # Input a text
+text = input("Enter a text: ")
+
 # Print space at the beginning of the text to complete the number of characters specifies in function parameter without using rjust() function
