5c486b5df5ba7ecc1a4b3106493df62a1c942e82
Chloe <lunamarychloeanne@gmail.com>
2025-04-01 14:14:20 +0800

    input

--- string_manipulation_program_03/print_the_position_where_the_character_was_located_starting_from_last_character_without_using_rindex.py
+++ string_manipulation_program_03/print_the_position_where_the_character_was_located_starting_from_last_character_without_using_rindex.py
@@ -1,3 +1,7 @@
 # Input a string
+string = input("Enter a string: ")
+
 # Input the character that you want to locate
+char_find = input("Enter the character that you want to locate: ")
+
 # Print The position where the last character was located
